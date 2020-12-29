8a8abacd9faee5104eab309903d7c89834c74103
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-12-29 11:07:17 +0900

    登録・ログイン機能追加

--- bookapp/routes/web.php
+++ bookapp/routes/web.php
@@ -41,3 +41,7 @@ Route::delete('/book/{book}', function(Book $book) {
 
     return redirect('/');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
